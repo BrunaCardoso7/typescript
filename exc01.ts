805d2eee8ba2d13314a761500ccad7d53cc6e800
BrunaCardoso7 <obitadrawing@gmail.com>
2023-08-10 08:52:46 -0300

    organizando ambiente de trabalho para typescript

---
+++ exc01.ts
@@ -0,0 +1,10 @@
+// Crie uma função que recebe um array de números e retorna a média dos valores.
+function sayMyName (nome: string) {
+	console.log("hello, wellcome "+nome.toUpperCase()) 
+}
+sayMyName('bruna')
+
+function whatsYourAge(): number { 
+    return 10
+}
+whatsYourAge()
